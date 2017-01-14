61f6187b919ef632c2b76adfa8de01a835a27eb8
Asif R Choudhury <choudhury.asif@gmail.com>
2017-01-14 12:02:52 +0600

    Revert "Add sayHelloNTimes"
    This reverts commit 15b1c944036cbdbc5ad5f3a4aaa4122f7f4fc39e.

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,11 +1,5 @@
 public class HelloWorld {
   public static void main(String[] args) {
-    sayHelloNTimes(10);
-  }
-
-  public static void sayHelloNTimes(int n) {
-    for (int i = 0; i < n; i++) {
-      System.out.println("Hello World!");
-    }
+    System.out.println("Hello World!");
   }
 }
